e6a82804745a27cb47a7210a98edaa133c176114
FabioCostaa <fabio.glz4@gmail.com>
2023-05-23 12:46:35 -0300

    first commit

---
+++ src/data/questions_complete.js
@@ -0,0 +1,119 @@
+const data = [
+  {
+    category: "Países",
+    questions: [
+      {
+        question: "Qual é o maior país em área territorial?",
+        options: ["Rússia", "Canadá", "China", "Estados Unidos"],
+        answer: "Rússia",
+        tip: "Possui território em dois continentes",
+      },
+      {
+        question: "Qual é o país mais populoso do mundo?",
+        options: ["Índia", "Estados Unidos", "Brasil", "China"],
+        answer: "China",
+        tip: "É um país ásiatico",
+      },
+      {
+        question: "Qual é o país que possui a cidade de Machu Picchu, uma antiga cidade inca?",
+        options: ["México", "Chile", "Peru", "Colômbia"],
+        answer: "Peru",
+        tip: "Possui fronteira com o Brasil",
+      },
+      {
+        question: "Qual é o país conhecido como Terra do Sol Nascente ?",
+        options: ["China", "Coreia do Sul", "Japão", "Tailândia"],
+        answer: "Japão",
+        tip: "O país também é conhecido pelos animes",
+      },
+      {
+        question: "Qual é o país que abriga a Grande Muralha?",
+        options: ["China", "Índia", "Peru", "Egito"],
+        answer: "China",
+        tip: "É um país ásiatico",
+      },
+    ],
+  },
+  {
+    category: "Brasil",
+    questions: [
+      {
+        question: "Qual é a capital do Brasil?",
+        options: ["Brasília", "São Paulo", "Rio de Janeiro", "Salvador"],
+        answer: "Brasília",
+        tip: "A cidade foi especialmente construída para ser a capital do país e é conhecida por sua arquitetura moderna.",
+      },
+      {
+        question: "Qual é o maior rio do Brasil?",
+        options: ["São Francisco", "Paraná", "Amazonas", "Tietê"],
+        answer: "Amazonas",
+        tip: "Fica na região norte do país",
+      },
+      {
+        question: "Qual é o nome da maior floresta tropical do mundo, localizada no Brasil?",
+        options: ["Mata Atlântica", "Pantanal", "Cerrado", "Floresta Amazônica"],
+        answer: "Floresta Amazônica",
+        tip: "Fica no estado de Amazonas",
+      },
+      {
+        question: "Qual é o monumento icônico localizado no topo do morro do Corcovado, no Rio de Janeiro?",
+        options: [
+          "Pão de Açúcar",
+          "Jardim Botânico",
+          "Maracanã",
+          "Cristo Redentor"],
+        answer: "Cristo Redentor",
+        tip: "É uma das Sete Maravilhas do Mundo Moderno",
+      },
+      {
+        question: "Qual é o estado mais populoso do Brasil?",
+        options: ["Minas Gerais", "Bahia", "São Paulo", "Rio Grande Do Sul"],
+        answer: "São Paulo",
+        tip: "O estado possui a maior economia do país",
+      },
+    ],
+  },
+  {
+    category: "Europa",
+    questions: [
+      {
+        question: "Qual é o país conhecido por sua torre inclinada?",
+        options: [
+          "Itália",
+          "Grécia",
+          "Holanda",
+          "Suécia",
+        ],
+        answer: "Itália",
+        tip:"Fica na cidade de Pisa.",
+      },
+      {
+        question: "Qual é o país que abriga o Museu do Louvre?",
+        options: ["Reino Unido", "Áustria", "França", "Suíça"],
+        answer: "França",
+        tip:"O Museu do Louvre, localizado em Paris, é um dos maiores e mais renomados museus do mundo.",
+      },
+      {
+        question: "Qual é a capital da Alemanha?",
+        options: ["Amsterdã", "Roma", "Paris", "Berlim"],
+        answer: "Berlim",
+        tip:"Já teve um muro que dividiu o país",
+      },
+      {
+        question: "Qual é o país que é famoso por seus moinhos de vento e tamancos?",
+        options: ["Suécia", "Noruega", "Holanda", "Bélgica"],
+        answer: "Holanda",
+        tip:"Eliminou o Brasil na copa de 2010",
+      },
+      {
+        question:
+          "Qual é o país que abriga o Parlamento Europeu?",
+        options: ["Alemanha", "Luxemburgo", "Bélgica", "França"],
+        answer: "Bélgica",
+        tip:"O Parlamento Europeu está localizado na cidade de Estrasburgo.",
+      },
+    ],
+  },
+];
+
+export default data;
